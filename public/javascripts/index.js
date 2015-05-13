29a2b14b2af0c9340b99e13bb8b5895dfa0bb89c
Zane Swafford <zane@zaneswafford.com>
2015-05-12 23:28:40 -0500

    We got a hacked-together calendar

---
+++ public/javascripts/index.js
@@ -0,0 +1,34 @@
+$(window).load(preparePage);
+
+function preparePage() {
+  $(".logo").addClass("animated bounceInUp");
+  $("#welcomeButtonContainer").addClass("animated bounceInUp");
+  $("#logInButton").click(onLogInButtonClicked);
+  $("#signUpButton").click(onSignUpButtonClicked);
+}
+
+function onLogInButtonClicked() {
+	if (!$("#logInForm").is(":visible")) {
+		$("#logInForm").slideDown();
+		$("#signUpForm").slideUp();
+		$("#logInButton").fadeOut();
+		$("#signUpButton").fadeOut();
+	} else {
+		$("#logInForm").slideUp();
+		$("#logInButton").fadeIn();
+		$("#signUpButton").fadeIn();
+	}
+}
+
+function onSignUpButtonClicked() {
+	if (!$("#signUpForm").is(":visible")) {
+		$("#signUpForm").slideDown();
+		$("#logInForm").slideUp();
+		$("#logInButton").fadeOut();
+		$("#signUpButton").fadeOut();
+	} else {
+		$("#signUpForm").slideUp();
+		$("#logInButton").fadeIn();
+		$("#signUpButton").fadeIn();
+	}
+}
